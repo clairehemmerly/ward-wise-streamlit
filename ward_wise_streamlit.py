0882267e58d9f86f69ef69abb86ffd2f9b885d34
cehemmerly <cehemmerly@uchicago.edu>
2023-09-05 09:20:08 -0500

    updated page layout

--- ward_wise_streamlit.py
+++ ward_wise_streamlit.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import json
 
-st.set_page_config(layout="wide")
+# st.set_page_config(layout="wide")
 
 #creat functions that will cache selected data
 @st.cache_data
@@ -34,22 +34,25 @@ cat_data, max_data, geometry = load_data()
 
 
 st.title('Ward-Wise')
-st.caption('Every year, Chicago alderpersons get $1.5 million to spend at their discretion on capital improvements on their ward.')
-url = "https://jonathanortega2023.github.io/alderman-spending/#/"
-st.write("To see a break down of spending in your ward go [here](%s)!" % url)
+st.write('Every year, Chicago alderpersons get $1.5 million to spend at their discretion on capital improvements on their ward.')
+option = st.radio('Select data to display:', ['Maximum spending by year', 'Ward spending by category'])
 
 with st.sidebar:
-    st.write("To find your ward number enter your address below")
+    url = "https://jonathanortega2023.github.io/alderman-spending/#/"
+    st.write("To see a break down of spending in your ward go [here](%s)!" % url)
+    st.write("Don't know you ward number? Enter your address below")
     st.components.v1.iframe(
     src="https://gisapps.chicago.gov/WardGeocode/?embed=true",
     height=450,
     width=400)
 
-#create columns for layout
-col1, col2 = st.columns(2)
 
-with col1:
-    st.header('Wards Spent the Most on...')
+#create columns for layout
+# col1, col2 = st.columns(2)
+
+# with col1:
+if option == "Maximum spending by year":
+    st.write('Choose a year to see where each ward spent the most money')
 
     year = st.selectbox(
         'Year',
@@ -59,28 +62,37 @@ with col1:
     max_spend = year_select(year)
 
     #create figure
-    fig2 = px.choropleth(max_spend, geojson=geometry, locations='ward', featureidkey="properties.ward",
+    fig = px.choropleth(max_spend, geojson=geometry, locations='ward', featureidkey="properties.ward",
                             color='category',
                             labels={'perc_spending': "Percent of Annual Spending"},
-                            hover_data={'perc_spending':':.1f'}
+                            hover_data={'perc_spending':':.1f'},
+                            height=300
                             )
 
-    fig2.update_geos(fitbounds="locations", visible=False)
-    fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
     #show figure
-    st.plotly_chart(fig2, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True)
 
-with col2:
-    st.header('By Category')
+# with col2:
+if option == "Ward spending by category":
+    st.write('Select a category to see how much was spent on it each year')
 
+    col1, col2 = st.columns(2)
+
+    with col1:
     #create drop down to select category
-    categories = cat_data['category'].unique()
-    category = st.selectbox(
-        'Spending Category',
-        (categories))    
-    
+        categories = cat_data['category'].unique()
+        category = st.selectbox(
+            'Spending Category',
+            (categories))  
 
+    with col2:
+        year = st.selectbox(
+        'Year',
+        (2022, 2021, 2020, 2019))  
+    
     #filter data by drop down selections
     plot_data = category_select(year, category)
 
@@ -89,7 +101,8 @@ with col2:
                             color='perc_spending',
                             color_continuous_scale="gnbu",
                             labels={'perc_spending': "Percent of Annual Spending"},
-                            hover_data={'perc_spending':':.1f'}
+                            hover_data={'perc_spending':':.1f'},
+                            height=300
                             )
     fig.update_geos(fitbounds="locations", visible=False)
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
